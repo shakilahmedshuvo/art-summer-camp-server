96c81942cb48bccbe0123199e7794b259ba3f439
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-06-09 02:01:28 +0600

    add reviews json file

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
+// json files for toys
+const instructorJSON = require('./instructor.json');
+const reviewsJSON = require('./review.json');
 
 // use middle ware
 app.use(cors());
@@ -10,6 +13,16 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Art Summer Camp is running')
+});
+
+// instructorJSON json file section
+app.get('/instructors', (req, res) => {
+    res.send(instructorJSON)
+});
+
+// reviewsJSON file section
+app.get('/reviews', (req, res) => {
+    res.send(reviewsJSON)
 });
 
 app.listen(port, () => {
