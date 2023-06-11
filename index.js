97f31d7b0502d89b69ce4ee9280a27509b1eca27
Shakil Ahmed <shakilahmedshuvo612@gmail.com>
2023-06-11 18:01:39 +0600

    post api and delete api for class

--- index.js
+++ index.js
@@ -127,6 +127,26 @@ async function run() {
             res.send(result);
         })
 
+        // 
+        // Select The Class get api
+        app.get('/selectClass', async (req, res) => {
+            const email = req.query.email;
+            if (!email) {
+                res.send([]);
+            }
+            const query = { email };
+            const result = await classCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        // cart delete api
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await classCollection.deleteOne(query)
+            res.send(result);
+        });
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
